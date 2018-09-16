a3b9ed73745b492defc91bb5f5c90a281ee3445a
oyron <oyron@statoil.com>
2018-09-16 21:59:20 +0200

    Hands-on part1

--- src/api.js
+++ src/api.js
@@ -7,19 +7,12 @@ const Library = require("./library");
 
 
 router.all('*', logRequest);
-router.get('/books', getBooks);
-router.post('/books', addBook);
 router.get('/books/:id', getBook);
-router.put('/books/:id', updateBook);
-router.delete('/books/:id', deleteBook);
+// ADD ENDPOINTS HERE
 router.use(unknownUrlHandler);
 router.use(errorHandler);
 
 const library = new Library();
-
-function getBooks(req, res) {
-    res.send(library.getAllBooks());
-}
 
 function getBook(req, res) {
     const bookId = Number(req.params.id);
@@ -31,35 +24,6 @@ function getBook(req, res) {
     }
 }
 
-function addBook(req, res) {
-    const bookData = req.body;
-    const book = library.addBook(bookData);
-    res.location(`/api/books/${book.id}`);
-    res.status(201).send(book);
-}
-
-function updateBook(req, res) {
-    const bookId = Number(req.params.id);
-    if (library.hasBookId(bookId)) {
-        const book = req.body;
-        library.updateBook(bookId, book);
-        res.send(book);
-    }
-    else {
-        res.status(404).send(`Book with id ${bookId} not found`);
-    }
-}
-
-function deleteBook(req, res) {
-    const bookId = Number(req.params.id);
-    if (library.hasBookId(bookId)) {
-        library.deleteBook(bookId);
-        res.sendStatus(204);
-    }
-    else {
-        res.status(404).send(`Book with id ${bookId} not found`);
-    }
-}
 
 function logRequest(req, res, next) {
     let payloadLog = "";
